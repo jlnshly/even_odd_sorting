df13dc2c87c0cab0d737f1075885725bd9b3417c
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 20:39:42 +0800

    code for functionalities done

--- functionalities.py
+++ functionalities.py
@@ -6,5 +6,11 @@ class ActivityTracker:
         self.start_time = datetime.now()
         self.activity_count = 0
 
-    def logactivity(self):
+    def log_activity(self, message: str):
         self.activity_count += 1
+        time_stamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'[{time_stamp}] {message}')
+
+    def get_runtime(self):
+        delta = datetime.now() - self.start_time
+        return f"{delta.total_seconds()} seconds"
