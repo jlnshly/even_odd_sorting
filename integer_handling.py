12eabb8b355cf316e6d60b173e4c4fb7475c017a
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 15:22:24 +0800

    progress for first class

--- integer_handling.py
+++ integer_handling.py
@@ -5,7 +5,14 @@ class IntegerHandling:
         self.integer = []
         self.integers = raw_integers
 
-    def numbers(self):
+    def integers(self):
         return self.integers
 
+    def integer(self, value):
+        sorted_integers = sorted(list(set([int(x) for x in value])))
+        self.integers = sorted_integers
+
+    def filter_parity_integers(self, parity: str = "even"): -> list[int]:
+        remainder = 0 if parity == "even" else 1
+        return [n for n]
 
