7cdbc51085ec48e6ffecaf8646a24c16911a6243
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 21:04:14 +0800

    main program progress

--- main_program.py
+++ main_program.py
@@ -1,3 +1,15 @@
 from integer_handling import IntegerHandling
 from functionalities import ActivityTracker
 from file_manager import FileManager
+
+class MainProgram(ActivityTracker):
+    def __init__(self):
+        super().__init__()
+        self.files = {
+            "source": "numbers.txt",
+            "even": "even.txt",
+            "odd": "odd.txt"
+        }
+
+    def execute(self):
+        
