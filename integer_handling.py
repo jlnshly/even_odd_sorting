bde93448d78ca07e14f037ef38fe1dfc1dc9ed24
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 15:09:07 +0800

    new function

--- integer_handling.py
+++ integer_handling.py
@@ -1,5 +1,10 @@
 class IntegerHandling:
+
     def __int__(self, raw_numbers: list):
         self.number = []
         self.numbers = raw_numbers
+
+    def numbers:
+        return self.numbers
+
 
