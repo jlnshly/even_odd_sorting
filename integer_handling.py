db66524a5c18124fd00a9c05e5f1d1d512a57aa5
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 21:47:34 +0800

    fixing errors

--- integer_handling.py
+++ integer_handling.py
@@ -1,9 +1,10 @@
 class IntegerHandling:
     """Class for handling raw even and odd integers"""
 
-    def __init__(self, raw_integers: list):
+    def __init__(self, raw_integers: list = None):
         self._integers = []
-        self.integers = raw_integers
+        if raw_integers:
+            self._integers = raw_integers
 
     @property
     def integers(self):
@@ -16,8 +17,8 @@ class IntegerHandling:
         sorted_integers = sorted(list(set([int(x) for x in value])))
         self._integers = sorted_integers
 
-    def filter_using_parity(self, parity: str="even"):
+    def filter_using_parity(self, parity: str='even'):
         """Filtering even and odd integers by parity"""
-        remainder = 0 if parity == "even" else 1
+        remainder = 0 if parity == 'even' else 1
         return [i for i in self._integers if i % 2 == remainder]
 
