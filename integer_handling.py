64de7ebb31bd263ad76bf66831263f3be425b7d7
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 15:25:17 +0800

    changed self.integer to self._integer to keep it as a private/raw variable

--- integer_handling.py
+++ integer_handling.py
@@ -2,17 +2,17 @@ class IntegerHandling:
     #Class for handling raw even and odd integers
 
     def __int__(self, raw_integers: list):
-        self.integer = []
+        self._integers = []
         self.integers = raw_integers
 
     def integers(self):
-        return self.integers
+        return self._integers
 
     def integer(self, value):
         sorted_integers = sorted(list(set([int(x) for x in value])))
-        self.integers = sorted_integers
+        self._integers = sorted_integers
 
     def filter_parity_integers(self, parity: str = "even"): -> list[int]:
         remainder = 0 if parity == "even" else 1
-        return [n for n]
+        return [n for n ]
 
