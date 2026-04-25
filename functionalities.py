637acc839035c00a501a699631f6891f7a232db8
jlnshly <jelaineashleyramos@gmail.com>
2026-04-25 20:35:09 +0800

    functionalities code progress

--- functionalities.py
+++ functionalities.py
@@ -3,3 +3,8 @@ from datetime import datetime
 class ActivityTracker:
 
     def __init__(self):
+        self.start_time = datetime.now()
+        self.activity_count = 0
+
+    def logactivity(self):
+        self.activity_count += 1
